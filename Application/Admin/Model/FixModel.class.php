134b10700a8cb3d3f72f880bf62eb5ac8c4c9dcf
myproject <382625199@qq.com>
2017-04-26 19:16:02 +0800

    1

---
+++ Application/Admin/Model/FixModel.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/4/26
+ * Time: 11:51
+ */
+
+namespace Admin\Model;
+
+
+use Think\Model;
+
+class FixModel extends Model
+{
+
+}
